f9797f7184cceaa6d3c6f089e9ec3e6f4e780f09
Gourab <gourabsahag@gmail.com>
2019-04-06 14:00:44 +0600

    TeachersProfile

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,17 +1,40 @@
+import 'package:Pathagar/login_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+
 class Profile extends StatelessWidget {
-  Profile(this.listType);
-  final String listType;
-  @override
-  Widget build(BuildContext context) {
+
+ build(BuildContext context)
+  {
+    Widget appBarTitle = new Text(
+      "Profile",
+      style: new TextStyle(color: Colors.white),
+    );
+
+    final Logout= Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(LoginPage.tag);
+        },
+        padding: EdgeInsets.all(12),
+        color: Colors.deepPurpleAccent,
+        child: Text('Log Out', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+
+
     return  SizedBox(height: 210,
       child: Card(
         child: Column(
           children: [
             ListTile(
-              title: Text('Mr.Abdul Kalam Azad',
+              title: Text('Mr.Quazi Ishtiaque Mahmud Rafi',
                  style: TextStyle(fontWeight: FontWeight.bold)),
               leading: Icon(
                 Icons.account_circle,
@@ -19,20 +42,34 @@ class Profile extends StatelessWidget {
               ),
             ),
             Divider(),
+
+
             ListTile(
-              title: Text('Donated Books: 5',
+              title: Text('Department: Software Engineering',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              leading: Icon(
+                Icons.dashboard,
+                color: Colors.deepPurple,
+              ),
+            ),
+
+            Divider(),
+
+            ListTile(
+              title: Text('Rank: Lecturer',
                   style: TextStyle(fontWeight: FontWeight.bold)),
               leading: Icon(
                 Icons.bookmark,
                 color: Colors.deepPurple,
               ),
             ),
+
             Divider(),
             ListTile(
-              title: Text('Borrowed Books: 2',
+              title: Text('Email: Rafi@sust.edu',
                   style: TextStyle(fontWeight: FontWeight.bold)),
               leading: Icon(
-                Icons.bookmark_border,
+                Icons.drafts,
                 color: Colors.deepPurple,
               ),
             ),
@@ -63,9 +100,21 @@ class Profile extends StatelessWidget {
                 color: Colors.deepPurple,
               ),
             ),
+            Divider(),
+            Logout,
+
           ],
+
         ),
+
+
+
       ),
+
     );
+
+
+
+
   }
 }
